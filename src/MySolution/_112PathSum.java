c2f3eae7bb32269a9228b5abd6110f164ccb54a2
yangtengfei <164722219@qq.com>
2016-03-13 21:13:27 +0800

    以后先做简单的

---
+++ src/MySolution/_112PathSum.java
@@ -0,0 +1,39 @@
+package MySolution;
+
+
+public class _112PathSum {
+	
+	public static void main(String[] args) {
+		TreeNode treeNode = new TreeNode(1);
+		TreeNode treeNode2 = new TreeNode(2);
+		treeNode.left = treeNode2;
+		boolean flag = hasPathSum(treeNode, 3);
+		System.out.println(flag);
+	}
+	
+	public static boolean hasPathSum(TreeNode root, int sum){
+		if (root == null) {
+			return false;
+		}
+		sum -= root.val;
+		if (root.left == null && root.right == null) {
+			if (sum == 0) {
+				return true;
+			}else {
+				return false;
+			}
+			
+		}
+		return hasPathSum(root.left, sum)||hasPathSum(root.right, sum);
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	
+	public TreeNode(int x){
+		val = x;
+	}
+}
