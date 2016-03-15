517442e64880e94d1e3c5c4f7f866396add8be20
yangtengfei <164722219@qq.com>
2016-03-15 14:49:28 +0800

    多次上传

---
+++ test/_237DeleteNodeInALinkedList.java
@@ -0,0 +1,18 @@
+/**
+ * leetcode的测试例子不足，
+ * node.val = node.next.val;
+ * node.next = node.next.next;
+ * 也能通过，不知道next.next是否存在
+ * @author yangtf
+ *
+ */
+public class _237DeleteNodeInALinkedList {
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		if (node.next.next == null) {
+			node.next = null;
+		} else {
+			node.next = node.next.next;
+		}
+	}
+}
