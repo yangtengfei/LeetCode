084dbc53227f2cfb9a86ff78093337178266042f
ytf <ytf@ytf-Rev-1-0>
2016-03-30 08:57:31 +0800

    191 ubuntu

--- src/UniqueBinarySearchTrees.java
+++ src/UniqueBinarySearchTrees.java
@@ -13,9 +13,6 @@ public class UniqueBinarySearchTrees {
 			return n;
 		}
 		else {
-			// 以i为根节点时，其左子树构成为[0,...,i-1],其右子树构成为[i+1,...,n]构成
-			// 因此，dp[i] = sigma（dp[0...k] * dp[k+1...i]） 0 <= k < i - 1 
-			// dp[3] = dp[0] * dp[2] + dp[1] * dp[1] + dp[2] * dp[0] 
 			int[] num = new int[n+1];
 			num[0] = num[1] = 1;
 			for (int i = 2; i < num.length; i++) {
