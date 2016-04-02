ff774da53330c43ec0e65804000fe10156c9ec21
yangtengfei <164722219@qq.com>
2016-04-02 10:55:01 +0800

    83

---
+++ test/_83RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,20 @@
+/**
+ * @author: tengfei yang
+ * @email: yangtengfei666666@163.com
+ * @version: 2016年4月2日上午10:36:55 
+ * @todo:Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * 1ms
+ */
+public class _83RemoveDuplicatesFromSortedList {
+	public ListNode deleteDuplicates(ListNode head) {
+		if (null != head) {
+			ListNode node = head;
+	        while (null != node.next) {
+	        	if (node.val != node.next.val)node = node.next;
+	        	else if (node.next.next == null)node.next = null;
+				else node.next = node.next.next;
+			}
+		}
+        return head;
+    }
+}
