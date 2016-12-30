31c3ed25252a14b1bd02deee55f8528bf7d18b15
yangtengfei01 <yangtengfei01@baidu.com>
2016-12-30 10:22:13 +0800

    刷了两道题gaga

---
+++ src/_20ValidParentheses.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ * @author baidu
+ *
+ */
+public class _20ValidParentheses {
+	
+	public static boolean isValid(String s){
+		Stack<Character> stack = new Stack<Character>();
+		for(char c : s.toCharArray()){
+			if(c == '(') stack.push(')');
+			else if(c == '[') stack.push(']');
+			else if(c == '{') stack.push('}');
+			else if (stack.isEmpty() || stack.pop() != c){
+				return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+	
+
+}
